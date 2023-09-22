5b4bec5d24f777fb00bf58181531dabdd7c4730b
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-09-22 09:19:13 -0500

    get all categories working as intended, tested with print statement in for statement and multiple categories are being stored and accessed so the tests are working

--- back-end/DAL/CategoryDAL/CategoryDALImplementation.py
+++ back-end/DAL/CategoryDAL/CategoryDALImplementation.py
@@ -11,7 +11,6 @@ class CategoryDALImplementation(CategoryDALInterface):
     def create_category(self, category: Category) -> Category:
         logging.info("Beginning DAL method create category with data: " + str(category.convert_to_dictionary()))
         sql = "INSERT INTO Category (category_name) VALUES (?) RETURNING category_id"
-        print(sql)
         connection = Connection.db_connection()
         cursor = connection.cursor()
         cursor.execute(sql, (category.category_name,))
@@ -23,7 +22,22 @@ class CategoryDALImplementation(CategoryDALInterface):
         return category
 
     def get_all_categories(self) -> List[Category]:
-        pass
+        logging.info("Beginning DAL method get all categories")
+        sql = "SELECT * FROM Category"
+        connection = Connection.db_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        category_records = cursor.fetchall()
+        categories = []
+        for category in category_records:
+            category = Category(*category)
+            categories.append(category)
+            logging.info("Finishing DAL method get all categories with result: "
+                         + str(category.convert_to_dictionary()))
+        cursor.close()
+        connection.commit()
+        connection.close()
+        return categories
 
     def update_category(self, category: Category) -> Category:
         pass
--- back-end/Tests/UnitTests/DALTests/CategoryDALTests.py
+++ back-end/Tests/UnitTests/DALTests/CategoryDALTests.py
@@ -7,3 +7,13 @@ test_category = Category(0, "test")
 def test_create_category_success():
     result = category_dao.create_category(test_category)
     assert result.category_id != 0
+
+def test_get_all_categories_success():
+    result = category_dao.get_all_categories()
+    assert len(result) > 0
+
+def test_update_category_success():
+    pass
+
+def test_delete_category_success():
+    pass
