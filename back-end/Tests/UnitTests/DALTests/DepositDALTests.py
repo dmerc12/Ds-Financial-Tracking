7ca665780bc710ed5b9d04a75403b7c2e60e93db
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-09-25 21:26:15 -0500

    deposit dal tests written and successfully failing while nothing is implemented, updated category dal tests to accomadate for the single test category

--- back-end/Tests/UnitTests/DALTests/DepositDALTests.py
+++ back-end/Tests/UnitTests/DALTests/DepositDALTests.py
@@ -9,13 +9,18 @@ current_deposit_id = 1
 updated_deposit = Deposit(current_deposit_id, test_deposit.category_id, test_deposit.date, 'updated', 50.00)
 
 def test_create_deposit_success():
-    pass
+    result = deposit_dao.create_deposit(test_deposit)
+    assert result.deposit_id != 0
 
 def test_get_all_deposits_success():
-    pass
+    result = deposit_dao.get_all_deposits()
+    assert len(result) > 0
 
 def test_update_deposit_success():
-    pass
+    result = deposit_dao.update_deposit(updated_deposit)
+    assert result.date != test_deposit.date and result.description != test_deposit.description and \
+           result.amount != test_deposit.amount
 
 def test_delete_deposit_success():
-    pass
+    result = deposit_dao.delete_deposit(current_deposit_id)
+    assert result
