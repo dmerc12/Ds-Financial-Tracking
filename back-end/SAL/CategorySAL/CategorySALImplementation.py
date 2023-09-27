d4d98d0b80eedcf1ec566480f7758fc8707413f7
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-09-27 16:52:25 -0500

    category sal interface complete and implementation set up ready for methods to be filled in after tests are created and initially fail

---
+++ back-end/SAL/CategorySAL/CategorySALImplementation.py
@@ -0,0 +1,23 @@
+import logging
+from typing import List
+
+from DAL.CategoryDAL.CategoryDALImplementation import CategoryDALImplementation
+from SAL.CategorySAL.CategorySALInterface import CategorySALInterface
+from Entities.Category import Category
+
+class CategorySALImplementation(CategorySALInterface):
+
+    def __init__(self, category_dao: CategoryDALImplementation):
+        self.category_dao = category_dao
+
+    def create_category(self, category: Category) -> Category:
+        pass
+
+    def get_all_categories(self) -> List[Category]:
+        pass
+
+    def update_category(self, category: Category) -> Category:
+        pass
+
+    def delete_category(self, category_id: int) -> bool:
+        pass
