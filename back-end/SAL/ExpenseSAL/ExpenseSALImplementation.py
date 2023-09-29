5af6a35b1e4a19819994eb5f6929fb2e3a997d50
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-09-28 20:16:55 -0500

    expense sal implementation set up after completing expense sal interface

---
+++ back-end/SAL/ExpenseSAL/ExpenseSALImplementation.py
@@ -0,0 +1,30 @@
+import logging
+from typing import List
+
+from DAL.ExpenseDAL.ExpenseDALImplementation import ExpenseDALImplementation
+from SAL.ExpenseSAL.ExpenseSALInterface import ExpenseSALInterface
+from SAL.CategorySAL.CategorySALImplementation import CategorySALImplementation
+from Entities.Expense import Expense
+from Entities.CustomError import CustomError
+
+class ExpenseSALImplementation(ExpenseSALInterface):
+
+    def create_expense(self, expense: Expense) -> Expense:
+        pass
+
+    def get_expense(self, expense_id: int) -> Expense:
+        pass
+
+    def get_all_expenses(self) -> List[Expense]:
+        pass
+
+    def update_expense(self, expense: Expense) -> Expense:
+        pass
+
+    def delete_expense(self, expense_id: int) -> bool:
+        pass
+
+    def __init__(self, expense_dao: ExpenseDALImplementation, category_sao: CategorySALImplementation):
+        self.expense_dao = expense_dao
+        self.category_sao = category_sao
+
---
+++ back-end/SAL/ExpenseSAL/ExpenseSALInterface.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from Entities.Expense import Expense
+
+class ExpenseSALInterface(ABC):
+
+    @abstractmethod
+    def create_expense(self, expense: Expense) -> Expense:
+        pass
+
+    @abstractmethod
+    def get_expense(self, expense_id: int) -> Expense:
+        pass
+
+    @abstractmethod
+    def get_all_expenses(self) -> List[Expense]:
+        pass
+
+    @abstractmethod
+    def update_expense(self, expense: Expense) -> Expense:
+        pass
+
+    @abstractmethod
+    def delete_expense(self, expense_id: int) -> bool:
+        pass
