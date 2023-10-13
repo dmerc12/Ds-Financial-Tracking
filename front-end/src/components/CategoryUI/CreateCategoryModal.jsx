d51f6f5c4f0f2fb6420c0d23aefb316940f14a40
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-12 21:20:31 -0500

    create category modal  complete

---
+++ front-end/src/components/CategoryUI/CreateCategoryModal.jsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { FaSpinner, FaSync } from 'react-icons/fa';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import { Modal } from '../Modal';
+
+export const CreateCategoryModal = ({ fetchCategories }) => {
+   const [categoryForm, setCategoryForm] = useState({categoryName: ''});
+   const [modalState, setModalState] = useState({
+      loading: false,
+      failedToFetch: false,
+      visible: false
+    });
+
+   const { fetchData } = useFetch();
+
+   const navigate = useNavigate();
+
+   const showModal = () => {
+      setModalState.visible(true);
+   };
+
+   const closeModal = () => {
+      setModalState.visible(false);
+   };
+
+   const goBack = () => {
+      setModalState.failedToFetch(false);
+   };
+
+   const onSubmit = async (event) => {
+      event.preventDefault();
+      setModalState((prevState) => ({
+         ...prevState,
+         loading: true,
+         failedToFetch: false
+      }))
+      try {
+         const { responseStatus, data } = await fetchData('/api/create/category', 'POST', categoryForm);
+
+         if (responseStatus === 201) {
+            setCategoryForm({categoryName: ''});
+            setModalState({
+               failedToFetch: false,
+               loading: false,
+               visible: false
+            });
+            fetchCategories();
+            toast.success("Category successfully created!", {toastId: 'customId'});
+         }
+      } catch (error) {
+         if (error.message === 'Failed to fetch') {
+            setModalState((prevState) => ({
+               ...prevState,
+               failedToFetch: true,
+               loading: false
+            }));
+         } else {
+            setModalState.loading(false);
+            toast.warn(error.message, {toastId: 'customId'});
+         }
+      }
+   };
+
+   return (
+       <>
+          <div className='component'>
+             <button onClick={showModal} className='action-btn' id='createCategoryModal'>Create Category</button>
+          </div>
+
+          <Modal visible={visible} onClose={closeModal}>
+             {modalState.loading ? (
+                <div className='loading-indicator'>
+                   <FaSpinner className='spinner' />
+                </div>
+             ) : modalState.failedToFetch ? (
+                <div className='failed-to-fetch'>
+                   <AiOutlineExclamationCircle className='warning-icon' />
+                   <p>Cannot connect to the back end server.</p>
+                   <p>Please check your internet connection and try again.</p>
+                   <button className='retry-button' onClick={onSubmit}>
+                      <FaSync className='retry-icon' />
+                   </button>
+                </div>
+             ) : (
+                <form className='form' onSubmit={onSubmit}>
+                  <div className='form-field'>
+                     <label className='form-label' htmlFor='categoryName'>Category Name: </label>
+                     <input className='form-input' type='text' name='categoryName' id='newCategoryName' value={categoryForm.categoryName} onChange={onChange} />
+
+                     <button className='form-btn-1' type='submit' id='createCategoryButton'>Create Category</button>
+                  </div>
+                </form>
+             )}
+          </Modal>
+       </>
+   );
+}
