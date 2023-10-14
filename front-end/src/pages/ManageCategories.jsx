8b2e77b44041c09a1eb6b03d1ffd5d4ff714e7e2
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-13 22:29:31 -0500

    adds category list component to the manage categories page

--- front-end/src/pages/ManageCategories.jsx
+++ front-end/src/pages/ManageCategories.jsx
@@ -1,7 +1,10 @@
+import { CategoryList } from "../components/CategoryUI/CategoryList"
+
 export const ManageCategories = () => {
     return (
         <>
-        
+            <h1>Manage Categories</h1>
+            <CategoryList />
         </>
     )
 }
