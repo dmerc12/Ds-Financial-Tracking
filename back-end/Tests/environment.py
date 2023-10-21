3d006a58464f6713865593a4add2845178ee251c
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-21 13:15:38 -0500

    environment file updated to include deposit and universal poms

--- back-end/Tests/environment.py
+++ back-end/Tests/environment.py
@@ -1,11 +1,15 @@
 from behave.runner import Context
 from selenium.webdriver.safari.webdriver import WebDriver
 # from selenium.webdriver.edge.webdriver import WebDriver
+from Tests.POMs.UniversalPOMs import UniversalPOMs
 from Tests.POMs.CategoryPOMs import CategoryPOMs
+from Tests.POMs.DepositPOMs import DepositPOMs
 
 def before(context: Context):
     context.driver = WebDriver()
+    context.universal_poms = UniversalPOMs(context.driver)
     context.category_poms = CategoryPOMs(context.driver)
+    context.deposit_poms = DepositPOMs(context.driver)
     context.driver.implicitly_wait(1)
 
 def after(context: Context):
