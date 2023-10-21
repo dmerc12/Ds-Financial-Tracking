d1d4c6aec8f78b23f2f263a05f0c25b70931a692
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-21 13:59:13 -0500

    updates category steps to include update category feature

--- back-end/Tests/Steps/CategorySteps.py
+++ back-end/Tests/Steps/CategorySteps.py
@@ -18,3 +18,18 @@ def step_impl(context, category_name):
 @when(u'I click the create category button')
 def step_impl(context):
     context.category_poms.click_create_category_button()
+
+
+@when(u'I click the update category modal on category {category_id}')
+def step_impl(context, category_id):
+    context.category_poms.click_update_category_modal(category_id)
+
+
+@when(u'I input {category_name} in the update category name input')
+def step_impl(context, category_name):
+    context.category_poms.input_update_category_name(category_name)
+
+
+@when(u'I click the update category button')
+def step_impl(context):
+    context.category_poms.click_update_category_button()
