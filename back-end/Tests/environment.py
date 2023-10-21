c39cdcd9c0206bbd1d0e143d55b2ce0a696f6418
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-21 15:06:14 -0500

    expense poms added to environment

--- back-end/Tests/environment.py
+++ back-end/Tests/environment.py
@@ -4,12 +4,14 @@ from selenium.webdriver.safari.webdriver import WebDriver
 from Tests.POMs.UniversalPOMs import UniversalPOMs
 from Tests.POMs.CategoryPOMs import CategoryPOMs
 from Tests.POMs.DepositPOMs import DepositPOMs
+from Tests.POMs.ExpensePOMs import ExpensePOMs
 
 def before(context: Context):
     context.driver = WebDriver()
     context.universal_poms = UniversalPOMs(context.driver)
     context.category_poms = CategoryPOMs(context.driver)
     context.deposit_poms = DepositPOMs(context.driver)
+    context.expense_poms = ExpensePOMs(context.driver)
     context.driver.implicitly_wait(1)
 
 def after(context: Context):
