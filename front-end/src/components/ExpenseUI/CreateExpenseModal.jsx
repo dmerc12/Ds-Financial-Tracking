7e605cc4ad939f54055d4468df7faa3fec6002c6
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-23 21:27:00 -0500

    creates all component files for expense UI

---
+++ front-end/src/components/ExpenseUI/CreateExpenseModal.jsx
@@ -0,0 +1,11 @@
+
+
+export const CreateExpenseModal = () => {
+
+
+    return (
+        <>
+        
+        </>
+    )
+}
---
+++ front-end/src/components/ExpenseUI/UpdateExpenseModal.jsx
@@ -0,0 +1,11 @@
+
+
+export const UpdateExpenseModal = () => {
+
+
+    return (
+        <>
+        
+        </>
+    )
+}
