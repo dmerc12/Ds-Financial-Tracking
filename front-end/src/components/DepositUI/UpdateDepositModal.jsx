2ae726f1c276fd29ab4eff77e2c9dcbb20c7715c
unknown <dylanmercer12@icloud.com>
2023-10-23 19:32:53 -0500

    progress made to update deposit modal but not yet complete

--- front-end/src/components/DepositUI/UpdateDepositModal.jsx
+++ front-end/src/components/DepositUI/UpdateDepositModal.jsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { toast } from 'react-toastify';
+import { FiEdit } from 'react-icons/fi';
+import { FaSpinner, FaSync } from 'react-icons/fa';
+import { AiOutlineExclamationCircle } from 'react-icons/ai';
+import { Modal } from '../Modal';
 
+export const UpdateDepositModal = ({ deposit, fetchDeposits }) => {
+    const [depositForm, setDepositForm] = useState({
+        depositId: deposit.depositId,
+        categoryId: deposit.categoryId,
+        date: deposit.date,
+        description: deposit.description,
+        amount: deposit.amount
+    });
+    const [visible, setVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [failedToFetch, setFailedToFetch] = useState(false);
 
-export const UpdateDepositModal = () => {
-    
+    const { fetchData } = useFetch();
+
+    const showModal = () => {
+        setVisible(true);
+    };
+
+    const closeModal = () => {
+        setVisible(false);
+    };
+
+    const goBack = () => {
+        setFailedToFetch(false);
+    };
+
+    const onChange = (event) => {
+        const { name, value } = event.target;
+        setDepositForm({
+            
+        })
+    }
 
     return (
         <>
