9666906f04f0f8e75d8f540fc7301313c6b643f0
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-23 21:21:45 -0500

    completes update deposit modal

--- front-end/src/components/DepositUI/UpdateDepositModal.jsx
+++ front-end/src/components/DepositUI/UpdateDepositModal.jsx
@@ -6,7 +6,7 @@ import { FaSpinner, FaSync } from 'react-icons/fa';
 import { AiOutlineExclamationCircle } from 'react-icons/ai';
 import { Modal } from '../Modal';
 
-export const UpdateDepositModal = ({ deposit, fetchDeposits }) => {
+export const UpdateDepositModal = ({ deposit, categories, fetchDeposits }) => {
     const [depositForm, setDepositForm] = useState({
         depositId: deposit.depositId,
         categoryId: deposit.categoryId,
@@ -34,14 +34,99 @@ export const UpdateDepositModal = ({ deposit, fetchDeposits }) => {
 
     const onChange = (event) => {
         const { name, value } = event.target;
-        setDepositForm({
-            
-        })
-    }
+        if (name === 'categoryId' | name === 'amount') {
+            setDepositForm((prevForm) => ({
+                ...prevForm,
+                [name]: Number(value)
+            }));
+        } else {
+            setDepositForm((prevForm) => ({
+                ...prevForm,
+                [name]: value
+            }));
+        }
+    };
+
+    const onDateChange = (date) => {
+        setDepositForm((prevForm) => ({
+            ...prevForm,
+            date: date
+        }));
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setFailedToFetch(false);
+        setLoading(true);
+        try {
+            const { responseStatus, data } = await fetchData('/api/update/deposit', 'PUD', depositForm);
+
+            if (responseStatus === 202) {
+                setLoading(false);
+                setVisible(false);
+                fetchDeposits();
+                toast.success("Deposit successfully updated!", {toastId: 'customId'});
+            } else if (responseStatus === 400) {
+                throw new Error(`${data.message}`);
+            } else {
+                throw new Error("Cannot connect to the back end server, please try again!");
+            }
+        } catch (error) {
+            if (error.message === 'Failed to fetch') {
+                setFailedToFetch(true);
+                setLoading(false);
+            } else {
+                setLoading(false);
+                toast.warn(error.message, {toastId: 'customId'});
+            }
+        }
+    };
 
     return (
         <>
-        
+            <FiEdit onClick={showModal} cursor='pointer' size={15} id={`updateDepositModal${deposit.depositId}`} />
+
+            <Modal visible={visible} onClose={closeModal}>
+                {loading ? (
+                    <div className='loading-indicator'>
+                        <FaSpinner className='spinner' />
+                    </div>
+                ) : failedToFetch ? (
+                    <div className='failed-to-fetch'>
+                        <AiOutlineExclamationCircle className='warning-icon' />
+                        <p>Cannot connect to the back end server.</p>
+                        <p>Please check your internet connection and try again.</p>
+                        <button className='retry-button' onClick={onSubmit}>
+                            <FaSync className='retry-icon' />
+                        </button>
+                        <button className='back-button' onClick={goBack}>Go Back</button>
+                    </div>
+                ) : (
+                    <form className='form' onSubmit={onSubmit}>
+                        <div className='form-field'>
+                            <label className='form-label' htmlFor='categoryId'>Category: </label>
+                            <select className='form-input' name='categoryId' id='updateDepositCategoryInput' value={depositForm.categoryId} onChange={onChange}>
+                                {categories && categories.length > 0 && (
+                                    categories.map(category => (
+                                        <option key={category.categoryId} value={category.categoryId}>{category.categoryName}</option>
+                                        ))
+                                )}
+                            </select>
+
+                            <label className="form-label" htmlFor="date">Date: </label>
+                            <DatePicker className="form-input" selected={depositForm.date} onChange={onDateChange} name="date" id="updateDepositDateInput" />
+
+                            <label className="form-label" htmlFor="description">Description: </label>
+                            <input className="form-input" type="text" name="description" id="updateDepositDescriptionInput" value={depositForm.description} onChange={onChange} />
+
+                            <label className="form-label" htmlFor="amount">Amount: </label>
+                            <input className="form-input" type="number" name="amount" id="updateDepositAmountInput" value={depositForm.amount} onChange={onChange} />
+
+                            <button className='form-btn-1' type='submit' id='updateDepositButton'>Update Deposit</button>
+                        </div>
+                    </form>
+                )}
+            </Modal>
         </>
     );
 }
