e24bda986ab477bc9cf9c6dafda4e96de4bbf395
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-31 12:22:25 -0500

    completes tests for new methods in expense dal tests

--- back-end/Tests/UnitTests/DALTests/ExpenseDALTests.py
+++ back-end/Tests/UnitTests/DALTests/ExpenseDALTests.py
@@ -22,10 +22,12 @@ def test_get_all_expenses_success():
     assert len(result) > 0
 
 def test_get_expenses_by_category_success():
-    pass
+    result = expense_dao.get_expenses_by_category(test_expense.category_id)
+    assert len(result) > 0
 
 def test_get_expenses_by_date_success():
-    pass
+    result = expense_dao.get_expenses_by_date(test_expense.date)
+    assert len(result) > 0
 
 def test_update_expense_success():
     result = expense_dao.update_expense(updated_expense)
