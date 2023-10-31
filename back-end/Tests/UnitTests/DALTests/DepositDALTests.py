7cf9c05d7ad32681de0f2d5186f5550079eebecf
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-10-30 20:29:30 -0500

    adds new but uncomplete tests for new methods in deposit dal tests

--- back-end/Tests/UnitTests/DALTests/DepositDALTests.py
+++ back-end/Tests/UnitTests/DALTests/DepositDALTests.py
@@ -21,6 +21,12 @@ def test_get_all_deposits_success():
     result = deposit_dao.get_all_deposits()
     assert len(result) > 0
 
+def test_get_deposits_by_category_success():
+    pass
+
+def test_get_deposits_by_date_success():
+    pass
+
 def test_update_deposit_success():
     result = deposit_dao.update_deposit(updated_deposit)
     assert result.date != test_deposit.date and result.description != test_deposit.description and \
