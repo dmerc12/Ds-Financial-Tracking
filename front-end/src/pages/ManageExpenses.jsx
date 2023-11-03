633db355e87d3fc65c0a887aa379191cc4661781
unknown <dylanmercer12@icloud.com>
2023-11-03 18:43:46 -0500

    updates pages to pass toast ref down into list components

--- front-end/src/pages/ManageExpenses.jsx
+++ front-end/src/pages/ManageExpenses.jsx
@@ -1,10 +1,16 @@
+import PropTypes from 'prop-types';
+
 import { ExpenseList } from '../components/ExpenseUI/ExpenseList';
 
-export const ManageExpenses = () => {
+export const ManageExpenses = ({ toastRef }) => {
     return (
         <>
             <h1>Manage Expenses</h1>
-            <ExpenseList />
+            <ExpenseList toastRef={toastRef}/>
         </>
     )
-}
+};
+
+ManageExpenses.propTypes = {
+    toastRef: PropTypes.object
+};
