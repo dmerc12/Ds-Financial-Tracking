fc9660015c0bae59ffe975f20d78e7b21d0f4d92
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-10 22:14:35 -0600

    adds session entity

---
+++ back-end/Entities/Session.py
@@ -0,0 +1,14 @@
+class Session:
+
+    def __init__(self, session_id: int, user_id: int, expires: datetime):
+        self.session_id = session_id
+        self.user_id = user_id
+        self.expires = expires
+
+    def convert_to_dictionary(self):
+        return {
+            'sessionId': self.session_id,
+            'userId': self.user_id,
+            'expires': self.expires
+        }
+    
