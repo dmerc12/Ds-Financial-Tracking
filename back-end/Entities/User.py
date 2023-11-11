76eea6ee169780c23cf0a2663bd03d4a60b52fb3
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-10 22:09:47 -0600

    adds user entity

---
+++ back-end/Entities/User.py
@@ -0,0 +1,16 @@
+class User:
+
+    def __init__(self, user_id: int, first_name: str, last_name: str, email: str, password: str):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def convert_to_dictionary(self):
+        return {
+            'userId': self.user_id,
+            'firstName': self.first_name,
+            'lastName': self.last_name,
+            'email': self.email
+        }
