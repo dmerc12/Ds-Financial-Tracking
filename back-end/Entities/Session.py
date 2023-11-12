d2279499188b305eb3f115774b3b49b1bfa51f57
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-11 23:05:50 -0600

    updates session entity

--- back-end/Entities/Session.py
+++ back-end/Entities/Session.py
@@ -1,13 +1,13 @@
 class Session:
 
-    def __init__(self, session_id: int, user_id: int, expires: datetime):
+    def __init__(self, session_id: int, user_id: int, expiration: datetime):
         self.session_id = session_id
         self.user_id = user_id
-        self.expires = expires
+        self.expiration = expiration
 
     def convert_to_dictionary(self):
         return {
             'sessionId': self.session_id,
             'userId': self.user_id,
-            'expires': self.expires
+            'expiration': self.expiration
         }
