1fb94d7eaf5dad90bb987df7cffff84019ed1ee0
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-11 23:39:08 -0600

    implements session dal implementation

--- back-end/DAL/SessionDAL/SessionDALImplementation.py
+++ back-end/DAL/SessionDAL/SessionDALImplementation.py
@@ -1,20 +1,78 @@
+import logging
+from datetime import datetime
+
 from DAL.SessionDAL.SessionDALInterface import SessionDALInterface
+from Database.config import Connection
 from Entities.Session import Session
 
 
 class SessionDALImplementation(SessionDALInterface):
 
     def create_session(self, session: Session) -> Session:
-        pass
+        logging.info("Beginning DAL method create session with session: " + session.convert_to_dictionary())
+        sql = "INSERT INTO financial_tracker.Session (user_id, expiration) VALUES (%s, %s) RETURNING session_id;"
+        connection = Connection.db_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (session.user_id, session.expiration))
+        connection.commit()
+        session_id = cursor.fetchone()[0]
+        cursor.close()
+        connection.commit()
+        connection.close()
+        session.session_id = session_id
+        logging.info("Finishing DAL method create session")
+        return session
 
     def get_session(self, session_id: int) -> Session:
-        pass
+        logging.info("Beginning DAL method get session with session ID: " + str(session_id))
+        sql = "SELECT * FROM financial_tracker.Session WHERE session_id=%s;"
+        connection = Connection.db_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (session_id,))
+        session_info = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        if session_info is None:
+            session = Session(0, 0, datetime(0000, 00, 00, 00, 00, 00, 00))
+            logging.info("Finishing DAL method get session, not found")
+            return session
+        else:
+            session = Session(*session_info)
+            logging.info("Finishing DAL method get session with session: " + str(session.convert_to_dictionary()))
+            return session
 
     def update_session(self, session: Session) -> Session:
-        pass
+        logging.info("Beginning DAL method update session with session: " + str(session.convert_to_dictionary()))
+        sql = "UPDATE financial_tracker.Session SET expiration=%s WHERE session_id=%s;"
+        connection = Connection.db_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (session.expiration, session.session_id))
+        cursor.close()
+        connection.commit()
+        connection.close()
+        logging.info("Finishing DAL method update_session with result: " + str(session.convert_to_dictionary()))
+        return session
 
     def delete_session(self, session_id: int) -> bool:
-        pass
+        logging.info("Beginning DAL method delete session with session ID: " + str(session_id))
+        sql = "DELETE FROM financial_tracker.Session WHERE session_id=%s;"
+        connection = Connection.db_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (session_id,))
+        cursor.close()
+        connection.commit()
+        connection.close()
+        logging.info("Finishing DAL method delete session")
+        return True
 
     def delete_all_sessions(self, user_id: int) -> bool:
-        pass
+        logging.info("Beginning DAL method delete all sessions with user ID: " + str(user_id))
+        sql = "DELETE FROM financial_tracker.Session WHERE user_id=%s;"
+        connection = Connection.db_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (user_id,))
+        cursor.close()
+        connection.commit()
+        connection.close()
+        logging.info("Finishing DAL method delete all sessions")
+        return True
