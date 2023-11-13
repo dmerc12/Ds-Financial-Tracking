4a8945ac5d37e2d6f2304123012c7b57c874abf1
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-12 21:08:31 -0600

    adds user sal and sets up implementation for tests

---
+++ back-end/SAL/UserSAL/UserSALImplementation.py
@@ -0,0 +1,25 @@
+from Entities.User import User
+from SAL.UserSAL.UserSALInterface import UserSALInterface
+
+
+class UserSALImplementation(UserSALInterface):
+    def create_user(self, user: User, password_confirmation: str) -> User:
+        pass
+
+    def get_user_by_id(self, user_id: int) -> User:
+        pass
+
+    def get_user_by_email(self, email: str) -> User:
+        pass
+
+    def login(self, email: str, password: str) -> User:
+        pass
+
+    def update_user(self, user: User) -> User:
+        pass
+
+    def change_password(self, user_id: int, new_password: str, password_confirmation: str) -> bool:
+        pass
+
+    def delete_user(self, user_id: int) -> bool:
+        pass
