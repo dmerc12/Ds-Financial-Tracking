c75f3b13b9a205f2771ebe6043c5efc4c6adcb9f
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-13 22:34:00 -0600

    adds session sal and sets up session sal implementation for tests

---
+++ back-end/SAL/SessionSAL/SessionSALInterface.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from Entities.Session import Session
+
+class SessionSALInterface(ABC):
+
+    @abstractmethod
+    def create_session(self, session: Session) -> Session:
+        pass
+
+    @abstractmethod
+    def get_session(self, session_id: int) -> Session:
+        pass
+
+    @abstractmethod
+    def delete_session(self, session_id: int) -> bool:
+        pass
+
+    @abstractmethod
+    def delete_all_sessions(self, user_id: int) -> bool:
+        pass
