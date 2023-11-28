a9cf73b799680f83326302c651306a4aef98f8d3
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2023-11-28 16:59:31 -0600

    fixes database scripts to work as intended

--- back-end/Database/ResetDatabase.py
+++ back-end/Database/ResetDatabase.py
@@ -25,16 +25,17 @@ if __name__ == "__main__":
     main_connection = Connection.db_connection()
     main_cursor = main_connection.cursor()
 
+    test_password = bcrypt.hashpw("test".encode(), bcrypt.gensalt()).decode()
     main_cursor.execute(f"INSERT INTO financial_tracker.User (user_id, email, passwrd) VALUES (-1, 'test@email.com', "
-                        f"'${bcrypt.hashpw('test'.encode(), bcrypt.gensalt())}');")
+                        f"'${test_password}');")
 
     main_cursor.execute(f"INSERT INTO financial_tracker.User (user_id, email, passwrd) "
                         f"VALUES (-2, 'deleteallsessions@email.com', "
-                        f"'${bcrypt.hashpw('test'.encode(), bcrypt.gensalt())}');")
+                        f"'${test_password}');")
 
-    main_cursor.execute("INSERT INTO financial_tracker.CATEGORY (category_id, group, user_id, category_name) VALUES "
+    main_cursor.execute("INSERT INTO financial_tracker.CATEGORY (category_id, user_id, grp, category_name) VALUES "
                         "(-1, -1, 'b', 'test category');")
-    main_cursor.execute("INSERT INTO financial_tracker.CATEGORY (category_id, group, user_id, category_name) VALUES "
+    main_cursor.execute("INSERT INTO financial_tracker.CATEGORY (category_id, user_id, grp, category_name) VALUES "
                         "(-2, -1, 'b', 'unused category');")
     print("Test category created successfully!")
 
