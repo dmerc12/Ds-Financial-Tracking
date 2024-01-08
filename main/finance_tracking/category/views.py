eb61ab889985362001258922bbf120145bd159b0
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-01-08 14:01:07 -0600

    adds empty template folders / files, adds views for category functionality

--- main/finance_tracking/category/views.py
+++ main/finance_tracking/category/views.py
@@ -18,3 +18,25 @@ def create_category(request):
         if form.is_valid():
             CategoryMiddleware.create_category(request, form)
             return redirect('category-list')
+    else:
+        form = CategoryForm()
+    return render(request, 'finance_tracking/category/create.html', {'form': form, 'action': 'create'})
+
+def update_review(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            CategoryMiddleware.update_category(request, form, category.pk)
+            return redirect('category-list')
+    else:
+        form = CategoryForm()
+    return render(request, 'finance_tracking/category/update.html', {'form': form, 'action': 'update', 'category': category})
+
+def delete_review(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    if request.method == 'POST':
+        CategoryMiddleware.delete_category(request, category)
+        return redirect('category-list')
+    return render(request, 'finance_tracking/category/delete.html', {'category': category})
+    
