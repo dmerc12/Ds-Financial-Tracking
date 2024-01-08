102f796df453fb800deead3b87b36c071074c942
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-01-08 12:00:08 -0600

    moves templates to main templates folder, adds middleware methods for category

--- main/finance_tracking/category/middleware.py
+++ main/finance_tracking/category/middleware.py
@@ -1,30 +1,34 @@
 import logging
-from typing import List
+from django.contrib import messages
 
 from ..models import Category
 
 class CategoryMiddleware:
 
     @staticmethod
-    def create_category():
-        pass
+    def create_category(request, form):
+        logging.info('Beginning middleware method create category')
+        category = Category()
+        category.name = form.cleaned_data['name']
+        category.group = form.cleaned_data['group']
+        category.user = request.user
+        category.save()
+        logging.info('Finishing middleware method create category')
+        messages.success(request, 'Category successfully created!')
 
     @staticmethod
-    def get_category():
-        pass
+    def update_category(request, form, category):
+        logging.info('Beginning middleware method update category')
+        category = Category.objects.get(pk=category.pk)
+        category.name = form.cleaned_data['name']
+        category.group = form.cleaned_data['group']
+        category.save()
+        logging.info('Finishing middleware method update category')
+        messages.success(request, 'Category successfully updated!')
 
     @staticmethod
-    def get_all_categorys():
-        pass
-
-    @staticmethod
-    def update_category():
-        pass
-
-    @staticmethod
-    def delete_category():
-        pass
-
-    @staticmethod
-    def delete_all_categorys():
-        pass
+    def delete_category(request, category):
+        logging.info('Beginning middleware method delete category')
+        category.delete()
+        logging.info('Finishing middleware method delete category')
+        messages.success(request, 'Category successfully deleted!')
