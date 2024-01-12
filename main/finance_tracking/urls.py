6259357dc9c75f87eff6f085914403d7cff85d03
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-01-11 23:24:15 -0600

    adds view for deposit home, create category button and drop down working so far

--- main/finance_tracking/urls.py
+++ main/finance_tracking/urls.py
@@ -2,17 +2,16 @@ from . import views
 from django.urls import path
 from django.contrib import admin
 from .category import views as category_views
-# from .deposit import views as deposit_views
+from .deposit import views as deposit_views
 # from .expense import views as expense_views
 
 urlpatterns = [
     # path('', views.home, name='finance-tracking-home'),
     path('categories', category_views.category_list, name='category-list'),
-    path('category/<int:category_id>/', category_views.category_detail, name='category-detail'),
     path('category/create/', category_views.create_category, name='create-category'),
     path('category/<int:category_id>/update', category_views.update_category, name='update-category'),
     path('category/<int:category_id>/delete', category_views.delete_category, name='delete-category'),
-    # path('deposits', deposit_views.deposit_list, name='deposit-list'),
+    path('deposits', deposit_views.home, name='deposit-home'),
     # path('deposit/<int:deposit_id>/', deposit_views.deposit_detail, name='deposit-detail'),
     # path('deposit/create/', deposit_views.create_deposit, name='create-deposit'),
     # path('deposit/<int:deposit_id/update', deposit_views.update_deposit, name='update-deposit'),
