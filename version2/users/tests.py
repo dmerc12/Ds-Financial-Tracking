ce1a6a23ab58ebf99c587d3ad6b85b2cd8b4a3bd
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-02-21 18:08:56 -0600

    adds tests for update user happy paths

--- version2/users/tests.py
+++ version2/users/tests.py
@@ -7,15 +7,16 @@ from django.urls import reverse
 class TestChangePasswordForm(TestCase):
     # Setup before the tests
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='old_password')
+        self.user = User.objects.create_user(username='testuser', password='old_password', first_name='test', last_name='user', email='testuser@example.com') 
 
-    # Test that the password is being initialized correctly
-    def test_form_initialization(self):
+    # Tests form initialization validation
+    def test_form_initialization_and_validation(self):
+        # Test form initialization
         form = ChangePasswordForm(user=self.user)
         self.assertNotIn('old_password', form.fields.keys())
 
-    # Tests form validation
-    def test_form_validation(self):
+        ## Test form validation
+
         # Test passwords not matching
         data = {
             'new_password1': 'new_password',
@@ -24,6 +25,52 @@ class TestChangePasswordForm(TestCase):
         form = ChangePasswordForm(user=self.user, data=data)
         self.assertFalse(form.is_valid())
         self.assertIn('new_password2', form.errors)
+
+        # Test empty passwords
+        data = {
+            'new_password1': '',
+            'new_password2': ''
+        }
+        form = ChangePasswordForm(user=self.user, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password2', form.errors)
+
+        # Test passwords contain username
+        data = {
+            'new_password1': 'testuser',
+            'new_password2': 'testuser'
+        }
+        form = ChangePasswordForm(user=self.user, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password2', form.errors)
+
+        # Test passwords contain first name
+        data = {
+            'new_password1': 'test',
+            'new_password2': 'test'
+        }
+        form = ChangePasswordForm(user=self.user, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password2', form.errors)
+
+        # Test passwords contain last name
+        data = {
+            'new_password1': 'user',
+            'new_password2': 'user'
+        }
+        form = ChangePasswordForm(user=self.user, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password2', form.errors)
+
+        # Test passwords contain email address
+        data = {
+            'new_password1': 'testuser@email.com',
+            'new_password2': 'testuser@email.com'
+        }
+        form = ChangePasswordForm(user=self.user, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_password2', form.errors)
+
         # Test success
         data['new_password2'] = 'new_password'
         form = ChangePasswordForm(user=self.user, data=data)
@@ -36,14 +83,18 @@ class TestUserView(TestCase):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='old_password')
 
-    # Test home view redirects if user not logged in
-    def test_home_view_redirect_if_not_logged_in(self):
+    # Teardown after tests
+    def tearDown(self):
+        User.objects.all().delete() 
+
+    # Test home view
+    def test_home_view(self):
+        # Test view redirects if user not logged in
         response = self.client.get(reverse('user-home'))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/login/?next=/users/')
 
-    # Test home view
-    def test_home_view(self):
+        # Test view with user logged in
         self.client.force_login(self.user)
         response = self.client.get(reverse('user-home'))
         self.assertEqual(response.status_code, 200)
@@ -55,6 +106,7 @@ class TestUserView(TestCase):
         response = self.client.get(reverse('add-user'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/register.html')
+
         # Test success
         data = {
             'first_name': 'John',
@@ -65,4 +117,31 @@ class TestUserView(TestCase):
             'password2': 'testpassword'
         }
         response = self.client.post(reverse('add-user'), data)
+        self.assertEqual(response.status_code, 302)
+
+    # Test update user view
+    def test_update_view(self):
+        # Test view redirects if user not logged in
+        response = self.client.get(reverse('update-user'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/login/?next=/users/update/')
+
+        # Test view with user logged in
+        self.client.force_login(self.user)
+
+        # Test rendering correct template when going to page
+        response = self.client.get(reverse('update-user'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/update.html')
+
+        # Test success
+        data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password1': 'testpassword',
+            'password2': 'testpassword'
+        }
+        response = self.client.post(reverse('update-user'), data)
         self.assertEqual(response.status_code, 302)
