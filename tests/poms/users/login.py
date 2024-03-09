c35cca56c37340e529ff86dbfd553d5bed04eded
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-03-08 22:19:08 -0600

    adds poms for login and change password pages

--- tests/poms/users/login.py
+++ tests/poms/users/login.py
@@ -10,10 +10,22 @@ class LoginPagePOMs:
         self.driver = driver
 
     # POM for clicking the register link on the login page
+    def click_register_link(self):
+        element: WebElement = self.driver.find_element(By.ID, 'registerLink')
+        return element.click()
 
     # POM for entering the username input on the login page
+    def enter_username_input(self, username):
+        element: WebElement = self.driver.find_element(By.ID, 'username')
+        return element.send_keys(username)
     
     # POM for entering the password input on the login page
+    def enter_password_input(self, password):
+        element: WebElement = self.driver.find_element(By.ID, 'password')
+        return element.send_keys(password)
         
     # POM for clicking the login button on the login page
+    def click_login_button(self):
+        element: WebElement = self.driver.find_element(By.ID, 'loginButton')
+        return element.click()
         
