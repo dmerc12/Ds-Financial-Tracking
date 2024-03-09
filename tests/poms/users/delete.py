414efee108c53c5ae452fbb8a886fa7c7ee9d073
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-03-08 22:23:13 -0600

    adds poms for delete user page

--- tests/poms/users/delete.py
+++ tests/poms/users/delete.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.safari.webdriver import WebDriver
 # from selenium.webdriver.edge.webdriver import WebDriver
 from selenium.webdriver.common.by import By
@@ -9,6 +10,12 @@ class DeleteUserPagePOMs:
         self.driver = driver
 
     # POM for clicking the back button on the delete user page
+    def click_back_button(self):
+        element: WebElement = self.driver.find_element(By.ID, 'backButton')
+        return element.click()
 
     # POM for clicking the delete user button on delete user page
+    def click_delete_user_button(self):
+        element: WebElement = self.driver.find_element(By.ID, 'deleteUserButton')
+        return element.click()
         
