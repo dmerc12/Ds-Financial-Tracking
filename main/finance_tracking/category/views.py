1f972718962a4008493da4116dfa173b0742bc13
unknown <dylanmercer12@icloud.com>
2024-04-07 15:01:27 -0500

    fixes issues with initally setting category group and update / delete buttons

--- main/finance_tracking/category/views.py
+++ main/finance_tracking/category/views.py
@@ -5,19 +5,20 @@ from .forms import CategoryForm
 from ..models import *
 
 # Create category view
-def create_category(request):
+def create_category(request, group):
     if request.user.is_authenticated:
         if request.method == 'POST':
             form = CategoryForm(request.POST)
             if form.is_valid():
                 category = form.save(commit=False)
                 category.user = request.user
+                category.group = group
                 category.save()
                 messages.success(request, 'Category successfully created!')
                 return redirect(f'{category.group}-home')
         else:
-            form = CategoryForm()
-        return render(request, 'finance_tracking/category/create.html', {'form': form, 'action': 'create'})
+            form = CategoryForm(initial={'group': group})
+        return render(request, 'finance_tracking/category/create.html', {'form': form, 'group': group})
     else:
         messages.error(request, 'You must be logged in to access this page. Please register or login then try again!')
         return redirect('login')
@@ -36,7 +37,7 @@ def update_category(request, category_id):
                 return redirect(f'{form.cleaned_data["group"]}-home')
         else:
             form = CategoryForm(initial={'group': group}, instance=category)
-        return render(request, 'finance_tracking/category/update.html', {'form': form, 'action': 'update', 'category': category})
+        return render(request, 'finance_tracking/category/update.html', {'form': form, 'category': category})
     else:
         messages.error(request, 'You must be logged in to access this page. Please register or login then try again!')
         return redirect('login')
